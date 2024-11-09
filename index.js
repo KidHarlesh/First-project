b2f5444629513c2c130d3d6ae5cb952aca726f64
KidHarlesh <123456789+odin@users.noreply.github.com>
2024-11-09 01:36:06 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const menulink = document.getElementById("menu-links");
+function change() {
+  menulink.classList.add("active");
+}
+
+function changeback() {
+  menulink.classList.remove("active");
+}
