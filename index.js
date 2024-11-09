e349797490b3a77106d45e50d875181cd398d6bf
KidHarlesh <123456789+odin@users.noreply.github.com>
2024-11-09 02:13:52 -0800

    new commit

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const menulink = document.getElementById("menu-links");
-function change() {
-  menulink.classList.add("active");
-}
+// const menulink = document.getElementById("menu-links");
+// function change() {
+//   menulink.classList.add("active");
+// }
 
-function changeback() {
-  menulink.classList.remove("active");
-}
+// function changeback() {
+//   menulink.classList.remove("active");
+// }
+
+
+window.onload = function () {
+  const menulink = document.getElementById("menu-links");
+
+  function change() {
+    menulink.classList.add("active");
+  }
+
+  function changeback() {
+    menulink.classList.remove("active");
+  }
+
+  // Assign the functions to the elements
+  document.querySelector(".fa-bars").onclick = change;
+  document.querySelector(".fa-close").onclick = changeback;
+};
+
